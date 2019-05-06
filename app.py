c9b9701d4da995c0bf6af7075a1e38e145c98e24
naveed-anwar123 <=naveedanwar152@gmail.com>
2019-05-06 14:55:26 +0500

    Fixed not importing db when running

--- app.py
+++ app.py
@@ -19,10 +19,6 @@ jwt = JWT(app, authenticate, identity)
 
 #flask decorator
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(ItemList, '/items')
